d8147792bcd0c4947b9fa00df5c1c36c46c8f5aa
etwicaksono <ekoteguhwicaksono@gmail.com>
2024-05-22 20:11:35 +0700

    init

---
+++ src/main/java/com/etwicaksono/btpnonlineshop/BtpnOnlineShopApplication.java
@@ -0,0 +1,13 @@
+package com.etwicaksono.btpnonlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtpnOnlineShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtpnOnlineShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/etwicaksono/btpnonlineshop/BtpnOnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etwicaksono.btpnonlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtpnOnlineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
