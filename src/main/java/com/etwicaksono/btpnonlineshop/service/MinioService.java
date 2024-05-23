19777f182ecf2edcce64ca383629e1c3ebcb7df4
etwicaksono <ekoteguhwicaksono@gmail.com>
2024-05-23 19:37:16 +0700

    Add MinioService

---
+++ src/main/java/com/etwicaksono/btpnonlineshop/service/MinioService.java
@@ -0,0 +1,46 @@
+package com.etwicaksono.btpnonlineshop.service;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.stereotype.Service;
+
+import io.minio.GetPresignedObjectUrlArgs;
+import io.minio.MinioClient;
+import io.minio.errors.ErrorResponseException;
+import io.minio.errors.InsufficientDataException;
+import io.minio.errors.InternalException;
+import io.minio.errors.InvalidResponseException;
+import io.minio.errors.ServerException;
+import io.minio.errors.XmlParserException;
+import io.minio.http.Method;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MinioService {
+   private final MinioClient minioClient;
+
+   private String getLink(String bucket, String filename, Long expiry)
+         throws InvalidKeyException, ErrorResponseException, InsufficientDataException, InternalException,
+         InvalidResponseException, XmlParserException, ServerException, IllegalArgumentException, IOException,
+         NoSuchAlgorithmException {
+      return minioClient.getPresignedObjectUrl(
+            GetPresignedObjectUrlArgs.builder()
+                  .method(Method.GET)
+                  .bucket(bucket)
+                  .object(filename)
+                  .expiry(Math.toIntExact(expiry), TimeUnit.SECONDS)
+                  .build());
+   }
+
+   public String generateMinioURL(String bucketName, String objectName) {
+      try {
+         return getLink(bucketName, objectName, 3600L);
+      } catch (Exception e) {
+         return null;
+      }
+   }
+}
