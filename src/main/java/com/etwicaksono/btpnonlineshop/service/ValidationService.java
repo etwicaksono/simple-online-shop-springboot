60791c2232f1b3fd34735cf51e7fc0396349732e
etwicaksono <ekoteguhwicaksono@gmail.com>
2024-05-23 19:20:47 +0700

    Add ValidationService

---
+++ src/main/java/com/etwicaksono/btpnonlineshop/service/ValidationService.java
@@ -0,0 +1,20 @@
+package com.etwicaksono.btpnonlineshop.service;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
+
+@Service
+public class ValidationService {
+   @Autowired
+   private Validator validator;
+
+   public void validate(Object object) {
+      Set<ConstraintViolation<Object>> violations = validator.validate(object);
+   }
+
+}
