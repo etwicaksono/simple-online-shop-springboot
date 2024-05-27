1e4a58d4ec7835d5b01fe69430b94b6ab73417be
etwicaksono <ekoteguhwicaksono@gmail.com>
2024-05-27 16:22:10 +0700

    Add ItemService interface

--- src/main/java/com/etwicaksono/btpnonlineshop/service/ItemService.java
+++ src/main/java/com/etwicaksono/btpnonlineshop/service/ItemService.java
@@ -1,5 +1,21 @@
 package com.etwicaksono.btpnonlineshop.service;
 
+import org.springframework.http.ResponseEntity;
+
+import com.etwicaksono.btpnonlineshop.dto.WebResponse;
+import com.etwicaksono.btpnonlineshop.dto.item.CreateItemRequest;
+import com.etwicaksono.btpnonlineshop.dto.item.GetListItemRequest;
+import com.etwicaksono.btpnonlineshop.dto.item.UpdateItemRequest;
+
 public interface ItemService {
+   ResponseEntity<WebResponse<Object>> createItem(CreateItemRequest request);
+
+   ResponseEntity<WebResponse<Object>> updateItem(UpdateItemRequest request);
+
+   ResponseEntity<WebResponse<Object>> findItem(Integer itemID);
+
+   ResponseEntity<WebResponse<Object>> deleteItem(Integer itemID);
+
+   ResponseEntity<WebResponse<Object>> getItem(GetListItemRequest request);
 
 }
